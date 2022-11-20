7b28105146f4fa4b09b5ede85035371176dd1405
Gabriel Da Silva Siqueira <99335926+GabrielSiqueir4@users.noreply.github.com>
2022-11-20 04:11:48 -0300

    .

---
+++ Prova/provaflutter/lib/COMPONENTS/auth_check.dart
@@ -0,0 +1,38 @@
+import 'package:atividade01/COMPONENTS/auth_service.dart';
+import 'package:atividade01/COMPONENTS/navigation.dart';
+import 'package:atividade01/PAGES/Home_Page.dart';
+import 'package:atividade01/PAGES/Login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+
+
+class AuthCheck extends StatefulWidget {
+  const AuthCheck({Key? key}) : super(key: key);
+
+  @override
+  State<AuthCheck> createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<AuthCheck> {
+  @override
+  Widget build(BuildContext context) {
+    AuthService auth = Provider.of<AuthService>(context);
+
+    if (auth.isLoading)
+      return loading();
+    else if (auth.usuario == null)
+      return LoginPage();
+    else
+      return Navigation();
+  }
+
+    loading() {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
--- Prova/provaflutter/lib/main.dart
+++ Prova/provaflutter/lib/main.dart
@@ -1,10 +1,21 @@
+import 'package:atividade01/COMPONENTS/auth_service.dart';
 import 'package:atividade01/COMPONENTS/navigation.dart';
+import 'package:atividade01/PAGES/Login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   await init();
-  runApp(const MyApp());
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 Future init() async {
@@ -19,7 +30,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Navigation(),
+      home: LoginPage(),
     );
   }
 }
